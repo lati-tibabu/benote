42bda1186b8818c273ee9b2ff217c0c27bf3db04
lati-tibabu <latitibabu2018@gmail.com>
2024-12-04 21:03:59 +0300

    backend: database syncronization with models endpoint implemented

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const sequelize = require('./models').sequelize;
 
 app.get('/', (req, res) => {
   res.send('Hello from the productivity hub!');
+});
+
+app.get('/database', (req, res) => {
+  sequelize.sync({alter: true})
+  .then(() => {
+    res.status(201).send('Database synced');
+  })
+  .catch(err => {
+    res.status(501).send('Error syncing database: ', err);
+    console.error('Error syncing', err);
+  })
 });
 
 module.exports = app;
