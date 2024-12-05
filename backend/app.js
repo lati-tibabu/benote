e6019926e9cb55813f2e091070b4b585f83677d3
lati-tibabu <latitibabu2018@gmail.com>
2024-12-05 09:53:29 +0300

    backend: routes has been included in the main app file

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const sequelize = require('./models').sequelize;
+const routes = require('./routes');
+
+app.use(express.json());
+app.use('/api', routes);
 
 app.get('/', (req, res) => {
   res.send('Hello from the productivity hub!');
