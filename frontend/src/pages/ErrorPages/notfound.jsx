0d9b6324a2e8fd043d6eb21e012a71a82b2e3daf
lati-tibabu <latitibabu2018@gmail.com>
2024-12-13 16:19:24 +0300

    fix: update error page and routing (notfound.jsx, index.jsx)

--- frontend/src/pages/ErrorPages/notfound.jsx
+++ frontend/src/pages/ErrorPages/notfound.jsx
@@ -5,15 +5,15 @@ const NotFound = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="max-w-md w-full text-center p-8 bg-white shadow-xl rounded-lg">
-        <h1 className="text-5xl font-bold text-red-600 mb-4">404</h1>
+        {/* <h1 className="text-5xl font-bold text-gray-80  0 mb-4">404</h1> */}
         <p className="text-xl font-semibold text-gray-700 mb-6">
           Oops! The page you are looking for doesn't exist.
         </p>
         <div className="flex justify-center mb-8">
           <img
-            src="https://img.icons8.com/ios/452/404.png"
+            src="/page_not_found.svg"
             alt="404 Error"
-            className="w-24 h-24"
+            className="w-44 h-44"
           />
         </div>
         <p className="text-gray-600 mb-6">
@@ -22,7 +22,7 @@ const NotFound = () => {
         </p>
         <a
           href="/"
-          className="btn btn-primary w-full md:w-48 mx-auto text-white hover:bg-blue-600 transition duration-300"
+          className="btn w-full md:w-48 mx-auto text-white bg-gray-800 hover:bg-gray-600 transition duration-300"
         >
           Go to Homepage
         </a>
--- frontend/src/routers/index.jsx
+++ frontend/src/routers/index.jsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import authRoutes from "./auth-router";
 import NotFound from "../pages/ErrorPages/notfound";
 import appRoutes from "./app-routes";
+import infoRoutes from "./info-routes";
 
 const router = createBrowserRouter([
   authRoutes,
   appRoutes,
+  infoRoutes,
   {
     path: "*",
     element: <NotFound />,
