3e5bed38999cd80d7dfb1e4be032aba1486d62c2
lati-tibabu <latitibabu2018@gmail.com>
2024-12-31 13:56:21 +0300

    feat: implement protected routes for dashboard and add new components for workspace, team, profile, and notification

--- frontend/src/routers/dashboard-routes.jsx
+++ frontend/src/routers/dashboard-routes.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+
 import Dashboard from "../pages/dashboard";
-import Home from "../pages/dashboard/contents/home";
+
+import Home from "../pages/dashboard/contents/Home";
+import Workspace from "../pages/dashboard/contents/Workspace";
+import Team from "../pages/dashboard/contents/Team";
+import Profile from "../pages/dashboard/contents/Profile";
+import Setting from "../pages/dashboard/contents/Setting";
+
+import ProtectedRoute from "./protected-routes";
+import Notification from "../pages/dashboard/contents/Notification";
 
 const dashboardRoutes = {
   path: "/app",
-  element: <Dashboard />,
+  // element: <Dashboard />,
+  element: <ProtectedRoute component={Dashboard} />,
   children: [
     { path: "home", element: <Home /> },
-    { path: "profile", element: <div>Profile</div> },
-    { path: "settings", element: <div>Settings</div> },
-    { path: "calendar", element: <div>Calendar</div> },
+    { path: "workspace", element: <Workspace /> },
+    { path: "team", element: <Team /> },
+    { path: "profile", element: <Profile /> },
+    { path: "notification", element: <Notification /> },
+    { path: "setting", element: <Setting /> },
   ],
 };
 
