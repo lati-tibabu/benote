cf11b38ae2f3772524e32394f57b71db9ff29956
lati-tibabu <latitibabu2018@gmail.com>
2025-02-21 11:42:52 +0300

    feat: add Redux workspace slice and configure store

---
+++ frontend/src/redux/slices/workspaceSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  workspace: {},
+}
+
+const workspaceSlice = createSlice({
+    name: 'workspace',
+    initialState,
+    reducers: {
+        setWorkspace: (state, action) => {
+            state.workspace = action.payload;
+        },
+        updateWorkspace: (state, action) => {
+            state.workspace = {...state.workspace, ...action.payload};
+        },
+        clearWorkspace: (state) => {
+            state.workspace = {};
+        },
+    },
+});
+
+export const { setWorkspace, updateWorkspace, clearWorkspace } = workspaceSlice.actions;
+export default workspaceSlice.reducer;
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit';
+import workspaceReducer from './slices/workspaceSlice';
+
+const store = configureStore({
+    reducer: {
+        workspace: workspaceReducer,
+    },
+});
+
+export default store;
