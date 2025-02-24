43401eb0bae19b351ba8f78f90530799fe61bb29
lati-tibabu <latitibabu2018@gmail.com>
2025-02-24 16:04:56 +0300

    feat: create migration for notifications table in the database

---
+++ backend/migrations/20250223114512-create-notification.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('notifications', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.UUID
+      },
+      message: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      action: {
+        type: Sequelize.JSON
+      },
+      receiver_id: {
+        type: Sequelize.UUID
+      },
+      sender_id: {
+        type: Sequelize.UUID
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('notifications');
+  }
+};
