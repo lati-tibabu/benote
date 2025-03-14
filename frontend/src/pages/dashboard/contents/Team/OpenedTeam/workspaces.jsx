b72f9ff7bd52d1228a48714b2c2cfb9206e453c1
lati-tibabu <latitibabu2018@gmail.com>
2025-03-14 11:02:52 +0300

    feat: add workspace creation button and modal for adding new workspaces

--- frontend/src/pages/dashboard/contents/Team/OpenedTeam/workspaces.jsx
+++ frontend/src/pages/dashboard/contents/Team/OpenedTeam/workspaces.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import AddNew from "../../Workspace/add_new";
 
 const TeamWorkspaces = () => {
   const apiURL = import.meta.env.VITE_API_URL;
@@ -20,10 +21,15 @@ const TeamWorkspaces = () => {
         method: "GET",
         headers: header,
       });
-      if (!response.ok)
+      if (!response.ok) {
+        // console.log(response.status);
+        // console.log(response.statusText);
         throw new Error("Error fetching workspace for the team");
+      }
 
       const data = await response.json();
+      console.log(data);
+
       setWorkspaces(data);
     } catch (error) {
       console.error(error);
@@ -48,6 +54,14 @@ const TeamWorkspaces = () => {
 
   return (
     <div className="p-4">
+      <div className="mb-2">
+        <button
+          className="btn btn-sm"
+          onClick={() => document.getElementById("my_modal_3").showModal()}
+        >
+          + Add Workspace
+        </button>
+      </div>
       {loading ? (
         <div>
           {[...Array(3)].map((_, index) => (
@@ -61,45 +75,40 @@ const TeamWorkspaces = () => {
         <div>No workspaces found</div>
       ) : (
         // <ul>
-
         <div>
           <ul className="flex flex-col gap-2 justify-stretch">
             {workspaces.map((workspace) => (
               <li>
                 <div
                   title={workspace.workspace.description}
-                  className="flex gap-2 items-center cursor-default border-1 border-black p-3 rounded-box hover:bg-gray-50 hover:border-r-4 hover:border-b-4 hover:cursor-pointer"
+                  className="flex gap-2 items-center cursor-default border-2 border-gray-300 p-3 rounded-box hover:bg-gray-50 hover:cursor-pointer hover:border-blue-500 hover:shadow transition-all duration-150"
                   onClick={() => handleWorkspaceOpen(workspace.workspace_id)}
                   key={workspace.workspace_id}
                 >
                   {/* icon */}
                   <div className="text-3xl">{workspace.workspace.emoji}</div>
                   {/* main */}
-                  <div className="border-l-1 pl-3">
-                    {/* name */}
-                    <div>{workspace.workspace.name}</div>
-                    {/* creation date */}
-                    <div className="font-bold text-sm">
-                      {new Date(workspace.workspace.createdAt).toDateString()}
+                  <div className="flex justify-between items-center w-full">
+                    <div className="border-l-1 pl-3">
+                      {/* name */}
+                      <div>{workspace.workspace.name}</div>
+                      {/* creation date */}
+                      <div className="font-bold text-sm text-gray-700">
+                        Created on{" "}
+                        {new Date(workspace.workspace.createdAt).toDateString()}
+                      </div>
+
+                      <div className="text-sm text-gray-700">
+                        Owned by{" "}
+                        <span className="hover:underline">
+                          {workspace?.workspace.creator?.name}
+                        </span>
+                      </div>
                     </div>
-                    {/* private or team */}
-                    {/* <div>
-                          {workspace.type.split("-")[0].toLowerCase().trim() ===
-                          "team" ? (
-                            <div className="flex items-center gap-1">
-                              <div className="bg-blue-600 text-white px-1 w-fit text-xs">
-                                {workspace.type.split("-")[0]}
-                              </div>
-                              <div className="text-xs hover:underline hover:text-blue-700">
-                                {workspace.type.split("-")[1]}
-                              </div>
-                            </div>
-                          ) : (
-                            <div className="bg-green-600 text-white px-1 w-fit text-xs">
-                              {workspace.type}
-                            </div>
-                          )}
-                        </div> */}
+
+                    <button className="btn btn-secondary btn-sm text-white bg-blue-600 hover:bg-blue-500  border-none outline-none ">
+                      Open
+                    </button>
                   </div>
                 </div>
               </li>
@@ -107,6 +116,17 @@ const TeamWorkspaces = () => {
           </ul>
         </div>
       )}
+
+      <dialog id="my_modal_3" className="modal overflow-x-scroll">
+        <div className="modal-box bg-white p-4 rounded-md shadow-md sm:w-fit lg:w-1/2 mx-auto mt-10 scrollbar-hide">
+          <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <AddNew teamId={team.id} />
+        </div>
+      </dialog>
     </div>
   );
 };
