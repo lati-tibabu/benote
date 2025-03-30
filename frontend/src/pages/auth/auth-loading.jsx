ff12da1a0caac1ff4bf344c9444acb260ef77741
lati-tibabu <latitibabu2018@gmail.com>
2025-03-30 16:50:15 +0300

    feat: implement user authentication handling in AuthLoading component

--- frontend/src/pages/auth/auth-loading.jsx
+++ frontend/src/pages/auth/auth-loading.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useNavigate, useLocation } from "react-router-dom";
+import { setAuthenticatedUser } from "../../redux/slices/authSlice";
+import { jwtDecode } from "jwt-decode";
 
 function AuthLoading() {
   const navigate = useNavigate();
   const location = useLocation();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
@@ -15,6 +19,13 @@ function AuthLoading() {
         "jwt_expiration",
         Date.now() + 30 * 24 * 60 * 60 * 1000
       );
+      const data = jwtDecode(token);
+      const authenticatedUser = {
+        id: data.id,
+        email: data.email,
+        name: data.name,
+      };
+      dispatch(setAuthenticatedUser(authenticatedUser));
 
       navigate("/app/home"); // Redirect to the protected route AFTER setting the token
     } else {
