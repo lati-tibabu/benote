f2f9f220514ab19a6515077d5a5c168f8759d431
lati-tibabu <latitibabu2018@gmail.com>
2025-04-16 11:45:52 +0300

    fix: correct authMiddleware import and update user routes to include authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const userControllers = require("../controllers/userControllers");
-const { authMiddleware } = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 router.post("/", userControllers.createUser);
 router.get("/", /*authMiddleware,*/ userControllers.readUsers);
 router.post("/email", userControllers.readUserByEmail);
 router.get("/:id", userControllers.readUser);
-router.put("/:id", userControllers.updateUser);
-router.delete("/:id", userControllers.deleteUser);
+router.put("/", authMiddleware.authMiddleware, userControllers.updateUser);
+router.delete("/", authMiddleware.authMiddleware, userControllers.deleteUser);
 
 module.exports = router;
