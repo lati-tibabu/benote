397ff1ff312c83cc419aa68cd41c9e66781fd50a
lati-tibabu <latitibabu2018@gmail.com>
2025-04-16 11:40:01 +0300

    feat: enhance updateUser and deleteUser functions to use authenticated user ID and improve password update logic

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -74,14 +74,56 @@ const readUserByEmail = async (req, res) => {
 // Update
 const updateUser = async (req, res) => {
   try {
-    const _user = await user.findByPk(req.params.id);
-    if (_user) {
-      await _user.update(req.body);
-      const updatedUser = { ..._user.get() }; // Use `.get()` to get raw data excluding Sequelize methods
-      res.json(updatedUser);
-    } else {
-      res.status(404).json({ message: "User not found!" });
+    const userId = req.user.id;
+    const _user = await user.findByPk(userId);
+
+    if (!_user) {
+      return res.status(404).json({ message: "User not found!" });
     }
+
+    const updatePassword = req.query.updatePassword;
+
+    if (updatePassword) {
+      if (_user.password_hash === null) {
+        return res.status(403).json({
+          message: "Password not set, most probably auth by Google",
+        });
+      }
+      const passwordData = req.body;
+
+      if (!passwordData) {
+        return res
+          .status(400)
+          .json({ message: "Password data is haha required" });
+      }
+
+      const isValidPassword = await bcrypt.compare(
+        passwordData.currentPassword,
+        _user.password_hash
+      );
+      if (!isValidPassword) {
+        return res
+          .status(403)
+          .json({ message: "Current password is incorrect" });
+      }
+
+      if (passwordData.newPassword !== passwordData.confirmPassword) {
+        return res.status(403).json({ message: "Passwords do not match" });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const newPassword = await bcrypt.hash(passwordData.newPassword, salt);
+      await _user.update({ password_hash: newPassword });
+      return res.json({ message: "Password updated successfully" });
+    }
+
+    await _user.update(req.body);
+    const updatedUser = { ..._user.get() }; // Use `.get()` to get raw data excluding Sequelize methods
+    res.json({
+      id: updatedUser.id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -90,13 +132,13 @@ const updateUser = async (req, res) => {
 // Delete
 const deleteUser = async (req, res) => {
   try {
-    const _user = await user.findByPk(req.params.id);
+    const userId = req.user.id;
+    const _user = await user.findByPk(userId);
     if (!_user) {
-      res.status(404).json({ message: "User not found!" });
-    } else {
-      await _user.destroy();
-      res.json({ message: "User deleted!" });
+      return res.status(404).json({ message: "User not found!" });
     }
+    await _user.destroy();
+    res.status(200).json({ message: "User deleted!" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
