b7bb92c673d83374ea77f0579be9a9be6efc56ad
lati-tibabu <latitibabu2018@gmail.com>
2025-05-15 08:25:58 +0300

    Add file upload middleware and integrate with resource routes

--- backend/routes/resourceRoutes.js
+++ backend/routes/resourceRoutes.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const resourceController = require("../controllers/resourceControllers");
+const upload = require("../middlewares/supabaseUpload");
 
 const authMiddleWare = require("../middlewares/authMiddleware");
 router.use(authMiddleWare.authMiddleware);
 
-router.post(
-  "/",
-  resourceController.uploadFile,
-  resourceController.createResource
-);
+router.post("/", upload.single("file"), resourceController.createResource);
 router.get("/", resourceController.getAllResources);
 router.get("/:id", resourceController.getResource);
 router.put("/:id", resourceController.updateResource);
 router.delete("/:id", resourceController.deleteResource);
+router.get("/signed-url/:fileName", resourceController.generateSignedUrl);
 
 module.exports = router;
